ab47e87106ce52fb6fb0f1f24c046672eb4e9733
anilkrishna1000 <66939047+anilkrishna1000@users.noreply.github.com>
2020-12-19 15:26:25 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask,render_template,url_for,request
+import numpy as np
+import pickle
+import pandas as pd
+import flasgger
+from flasgger import Swagger
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import  GridSearchCV
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.externals import joblib
+from sklearn.exceptions import NotFittedError
+import joblib
+
+app=Flask(__name__)
+Swagger(app)
+
+rf = pickle.load(open('rf_model.pkl','rb'))
+res = pickle.load(open('res_vect.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    if request.method == 'POST':
+        review = request.form['review']
+        data = [review]
+        vect =  res.transform(data)
+        my_prediction = rf.predict(vect)
+    return render_template('result.html',prediction = my_prediction)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
